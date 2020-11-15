91fce12ea168e5421115f96a057477cd78e7a8ca
Or <or.dicker@gmail.com>
2020-11-15 14:47:17 +0200

    add conv2d for image derivatives

--- ctypi.py
+++ ctypi.py
@@ -1,8 +1,25 @@
 import numpy as np
-import torch 
-from PIL import Image # loading images
+import torch
+import torch.nn.functional as F
+# loading images
+from PIL import Image
 from os import listdir
 
+x_filter = torch.Tensor([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+y_filter = x_filter.transpose(0,1)
+
+def conv2(img, filt):
+    """
+    filter image using pytorch
+    img - input image [H, W]
+    filter - Tensor [f_H, f_W]
+    TODO: check the padding
+    """
+    H,W = img.size()
+    f_H,f_W = filt.size()
+    return F.conv2d(img.expand(1,1,H,W),
+                    filt.expand(1,1,f_H,f_W),
+                    padding=((f_H-1)//2,(f_W-1)//2)).squeeze()
 
 def align(stack):
     """
@@ -10,11 +27,10 @@ def align(stack):
     output - align stack
     """
 
-
 def merge(stack):
     """
-    stack - align stack of images [batch, width, height]
-    output - single image [width, height]
+    stack - align stack of images [batch, height, width]
+    output - single image [height ,width]
     """
 
 
@@ -26,14 +42,9 @@ def load_images(path, N):
     """
     images_files = listdir(path)
     images = [path+img for img in images_files if img.endswith(".tif")]
-    stack = np.asarray([np.array(Image.open(img)) for img in images], dtype=np.int32)
+    stack = np.asarray([np.array(Image.open(img)) for img in images], dtype=np.float32) # have to be float32 for conv2d input 
     return torch.from_numpy(stack)
-    
-    
-        
-    
+
 
 if __name__ == '__main__':
     print("hello world!")
-    
-
